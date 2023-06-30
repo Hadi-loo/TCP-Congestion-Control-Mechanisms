af5355d71b4de5d88f13dee79212ef3072bb0233
Alireza-Zwolf <alireza2621@gmail.com>
2023-06-30 10:31:29 +0330

    Remove some comments from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,21 @@ int main(){
     
     // TCP Reno with 1 packet loss
     // connection1.sendData();
-    // connection1.onPacketLoss(TCP_RENO, 1);
+    connection1.onPacketLoss(TCP_RENO, 1);
 
     // Updating RTT
     connection1.onRTTUpdate(2);
 
     // TCP Reno with 3 packet loss
     // connection1.sendData(false, 3);
-    // connection1.onPacketLoss(TCP_RENO, 3);
+    connection1.onPacketLoss(TCP_RENO, 3);
 
     // Updating RTT
     connection1.onRTTUpdate(3);
 
     // TCP New Reno with 3 packet loss
     // connection1.sendData(true, 3);
-    // connection1.onPacketLoss(TCP_NEW_RENO, 3);
+    connection1.onPacketLoss(TCP_NEW_RENO, 3);
 
     // Updating RTT
     connection1.onRTTUpdate(1);
